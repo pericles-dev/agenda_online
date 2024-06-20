0b1b46d7701d7f5d02f79fb3777be8cf2877a583
Péricles Santos <periclessantanadossantos@hotmail.com>
2024-06-20 20:43:16 -0300

    Melhorias e atualização no sistema de views

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -1,12 +1,29 @@
 <?php
+
 namespace app\controllers;
 
 use app\controllers\BaseController;
 
 class Main extends BaseController
 {
+
+    public function home()
+    {
+        $this->view("layouts/header_template");
+        echo "<h1>Home</h1>";
+        $this->view("layouts/footer_template");
+    }
     public function login()
     {
+        $this->view("layouts/header_template");
         $this->view("login_form");
+        $this->view("layouts/footer_template");
+    }
+
+    public function register()
+    {
+        $this->view("layouts/header_template");
+        $this->view("register_form");
+        $this->view("layouts/footer_template");
     }
 }
--- app/system/Router.php
+++ app/system/Router.php
@@ -2,15 +2,16 @@
 
 namespace app\system;
 
+
 class Router
 {
     static public function route()
     {
         $routes = explode("/", $_SERVER["REQUEST_URI"]);
 
-        $controller_name = isset($routes[1]) ? ucfirst($routes[1]) : "main";
-        $method = isset($routes[2]) ? $routes[2] : "index";
-        $id = isset($routes[3]) ? $routes[3] : null;
+        $controller_name = !empty($routes[1]) ? ucfirst($routes[1]) : "Main";
+        $method = !empty($routes[2]) ? $routes[2] : "home";
+        $id = !empty($routes[3]) ? $routes[3] : null;
 
         try {
             $class = "app\controllers\\$controller_name";
