be8d9a6567d103935caefc9057571d13621c115b
Péricles Santos <periclessantanadossantos@hotmail.com>
2024-06-21 20:13:39 -0300

    alterações nos formulários de login e registro

--- app/views/login_form.php
+++ app/views/login_form.php
@@ -1,1 +1,14 @@
-<h1>Login</h1>
+<form action="/user/login" method="get">
+
+    <label for="email">
+        <span class="input-title">E-mail:</span>
+        <input type="email" name="email" id="email">
+    </label>
+
+    <label for="password">
+        <span class="input-title">Senha:</span>
+        <input type="password" name="password" id="password">
+    </label>
+    
+    <input type="submit" value="Entrar">
+</form>
--- app/views/register_form.php
+++ app/views/register_form.php
@@ -1,1 +1,46 @@
-<h1>Register</h1>
+<form action="/user/register" method="post">
+    <label for="first_name">
+        <span class="input-title">Nome:</span>
+        <input type="text" name="first_name" id="first_name">
+    </label>
+
+    <label for="last_name">
+        <span class="input-title">Sobrenome:</span>
+        <input type="text" name="last_name" id="last_name">
+    </label>
+
+    <label for="username">
+        <span class="input-title">Usuário:</span>
+        <input type="text" name="username" id="user_name">
+    </label>
+
+    <label for="birthdate">
+        <span class="input-title">Data de nascimento:</span>
+        <input type="date" name="birthdate" id="birthdate">
+    </label>
+
+    <label for="email">
+        <span class="input-title">Gênero:</span>
+        <span>
+            Masculino:
+            <input type="radio" name="gender" value="male">
+        </span>
+        <span>
+            Feminino:
+            <input type="radio" name="gender" value="female">
+        </span>
+        
+    </label>
+
+    <label for="email">
+        <span class="input-title">E-mail:</span>
+        <input type="email" name="email" id="email">
+    </label>
+
+    <label for="confirm_email">
+        <span class="input-title">E-mail:</span>
+        <input type="email" name="confirm_email" id="confirm_email">
+    </label>
+
+    <input type="submit" value="Registrar">
+</form>
